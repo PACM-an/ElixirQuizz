12797ac05ecd3b859171c0b8238114ec0f659979
PACM-an <pabloacoca@gmail.com>
2024-02-19 22:35:05 -0600

    Prueba de actualizacion envio

--- lib/quizz.ex
+++ lib/quizz.ex
@@ -1,18 +1,6 @@
 defmodule Quizz do
-  @moduledoc """
-  Documentation for `Quizz`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Quizz.hello()
-      :world
-
-  """
   def hello do
-    :world
+
   end
 end
